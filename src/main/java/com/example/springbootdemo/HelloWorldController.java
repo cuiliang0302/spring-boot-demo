74be3496375c7c4fa112aecff01d320b951305c3
Liang.Cui <cuiliang0302@qq.com>
2024-07-20 22:27:51 +0800

    新增kustomization

--- src/main/java/com/example/springbootdemo/HelloWorldController.java
+++ src/main/java/com/example/springbootdemo/HelloWorldController.java
@@ -8,7 +8,9 @@ public class HelloWorldController {
     @RequestMapping("/")
     @ResponseBody
     public String hello() {
-        return "<h1>Hello SpringBoot</h1><p>Version:v1 Env:prod</p>";
+        // 获取环境变量 ENV_NAME，如果不存在则使用默认值 "default"
+        String envName = System.getenv().getOrDefault("ENV_NAME", "default");
+        return String.format("<h1>Hello SpringBoot</h1><p>Version:v1 Env:%s</p>", envName);
     }
     @RequestMapping("/health")
     @ResponseBody
